c739c94789e22357946e614264373ff8df54146e
BurnetteZ <165413666+BurnetteZ@users.noreply.github.com>
2025-09-11 21:56:09 +0200

    Include project templates directory in Django settings

--- bodha_base/settings.py
+++ bodha_base/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'bodha_base.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
